af8cea8ea154425c4a4adc697e532168bdaea668
mperry <perrygeo@gmail.com>
2022-06-01 10:49:54 -0600

    error metric

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -2,6 +2,7 @@
 //!
 use crate::{statistics::RequestMetric, strategies::WebMapSession};
 use log::{debug, error};
+use reqwest::StatusCode;
 use std::time::Instant;
 use tokio::sync::mpsc;
 
@@ -13,36 +14,37 @@ pub async fn request_handler(
 ) {
     let (url, tile) = strategy.make_url(seed);
     let start = Instant::now();
-    let res = reqwest::get(url).await;
+    let res = reqwest::get(&url).await;
     debug!("{:?}", res);
     let duration = start.elapsed();
 
-    if let Ok(response) = res {
+    let metric = if let Ok(response) = res {
+        // success!
         // Parse stats from response
         let status = response.status();
         let path = response.url().to_string();
         let headers = response.headers().clone();
+        let content_type = headers.get("Content-Type");
         let content_length = if let Some(length) = response.content_length() {
             length
         } else {
             response.bytes().await.unwrap().len() as u64
         };
-        let content_type = headers.get("Content-Type");
 
-        // Send stats
-        let stat = RequestMetric::new(
+        RequestMetric::new(
             path,
             status,
             content_length,
             duration,
             content_type,
             tile.zoom,
-        );
-        tx_stats.send(stat).await.unwrap();
+        )
     } else {
-        // TODO send failed metric
-        // let stat = RequestMetric::new(path, status, None, duration, None);
-        // tx_stats.send(stat).await.unwrap();
-        error!("{:?}", res);
+        // failed :-(
+        // TODO inspect the error and make sure that
+        // 502 Bad Gateway is an appropriate status code
+        RequestMetric::new(url, StatusCode::BAD_GATEWAY, 0, duration, None, tile.zoom)
     };
+
+    tx_stats.send(metric).await.unwrap();
 }
